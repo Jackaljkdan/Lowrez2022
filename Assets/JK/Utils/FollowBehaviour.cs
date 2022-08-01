8f2e9f18ed85b16935614cb4cde61e1d831e4fae
Daniele Cortesi <dani.c93@hotmail.it>
2022-08-01 22:52:04 +0200

    Account for 64x64

---
+++ Assets/JK/Utils/FollowBehaviour.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JK.Utils
+{
+    public abstract class FollowBehaviour : MonoBehaviour
+    {
+        #region Inspector
+
+        public Transform target;
+
+        #endregion
+    }
+}
