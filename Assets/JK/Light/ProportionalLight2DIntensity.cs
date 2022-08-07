baf4fff62e9977f4c3deccb7de662fc047df433a
Daniele Cortesi <dani.c93@hotmail.it>
2022-08-07 09:51:27 +0200

    Regulate player light proportionally to flashlight

---
+++ Assets/JK/Light/ProportionalLight2DIntensity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace JK.Lighting
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Light2D))]
+    public class ProportionalLight2DIntensity : MonoBehaviour
+    {
+        #region Inspector
+
+        public Light2D target;
+
+        #endregion
+
+        private float maxIntensity;
+        private float targetMaxIntensity;
+
+        private void Awake()
+        {
+            maxIntensity = GetComponent<Light2D>().intensity;
+            targetMaxIntensity = target.intensity;
+        }
+
+        private void LateUpdate()
+        {
+            GetComponent<Light2D>().intensity = maxIntensity * target.intensity / targetMaxIntensity;
+        }
+    }
+}
