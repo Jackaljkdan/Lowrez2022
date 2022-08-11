bab288a6143f80fa702c676c6ec8d3c2257ef2fd
Daniele Cortesi <dani.c93@hotmail.it>
2022-08-11 20:52:49 +0200

    Fix room enabler for large rooms

--- Assets/Lowrez/Rooms/RoomEnabler.cs
+++ Assets/Lowrez/Rooms/RoomEnabler.cs
@@ -17,16 +17,43 @@ namespace Lowrez.Rooms
 
         #endregion
 
+        private Dictionary<Room, int> refenceCount;
+
+        private void Awake()
+        {
+            refenceCount = new Dictionary<Room, int>(8);
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.TryGetComponent(out RoomFitterTrigger trigger))
+            {
                 trigger.room.gameObject.SetActive(true);
+
+                if (refenceCount.ContainsKey(trigger.room))
+                    refenceCount[trigger.room] += 1;
+                else
+                    refenceCount[trigger.room] = 1;
+            }
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
             if (collision.TryGetComponent(out RoomFitterTrigger trigger))
-                trigger.room.gameObject.SetActive(false);
+            {
+                if (!refenceCount.TryGetValue(trigger.room, out int count))
+                    count = 0;
+
+                if (count > 1)
+                {
+                    refenceCount[trigger.room] -= 1;
+                }
+                else
+                {
+                    refenceCount[trigger.room] = 0;
+                    trigger.room.gameObject.SetActive(false);
+                }
+            }
         }
     }
 }
