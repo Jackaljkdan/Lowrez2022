a85584278d65d51e6bf781ca9aabaaaf08c95b39
Daniele Cortesi <dani.c93@hotmail.it>
2022-08-12 19:55:33 +0200

    Add footstep sounds

--- Assets/JK/Actuators/RigidBody2DMovementActuator.cs
+++ Assets/JK/Actuators/RigidBody2DMovementActuator.cs
@@ -7,43 +7,13 @@ using UnityEngine.Events;
 namespace JK.Actuators
 {
     [DisallowMultipleComponent]
-    public class RigidBody2DMovementActuator : MonoBehaviour, IMovementActuator
+    public class RigidBody2DMovementActuator : MovementActuatorBehaviour
     {
         #region Inspector
 
-        [SerializeField]
-        private float _speed = 3;
-
-        public Transform _directionReference;
-
         public bool clampInput = true;
 
-        [field: SerializeField, Header("Runtime")]
-        public Vector3 Input { get; set; }
-
-        [SerializeField]
-        private UnityEvent<Vector3> _onMovement = new UnityEvent<Vector3>();
-
-        private void Reset()
-        {
-            DirectionReference = transform;
-        }
-
         #endregion
-
-        public float Speed
-        {
-            get => _speed;
-            set => _speed = value;
-        }
-
-        public Transform DirectionReference
-        {
-            get => _directionReference;
-            set => _directionReference = value;
-        }
-
-        public UnityEvent<Vector3> onMovement => _onMovement;
 
         private void Start()
         {
--- Assets/JK/Utils/AudioSourceUtils.cs
+++ Assets/JK/Utils/AudioSourceUtils.cs
@@ -13,5 +13,23 @@ namespace JK.Utils
             if (source != null && clip != null)
                 source.PlayOneShot(clip);
         }
+
+        public static AudioClip PlayRandomClip(this AudioSource source, List<AudioClip> clips, bool oneShot)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, clips.Count);
+            AudioClip randomClip = clips[randomIndex];
+
+            if (oneShot)
+            {
+                source.PlayOneShot(randomClip);
+            }
+            else
+            {
+                source.clip = randomClip;
+                source.Play();
+            }
+
+            return randomClip;
+        }
     }
 }
